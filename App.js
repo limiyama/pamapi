c2dcad4bd69b368b6d048181e410591a33a8c121
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-07-28 14:51:08 -0300

    API

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View>
+          <Text style={styles.titulo}> Previsão do Tempo </Text>
+        </View>
+        <View style={styles.bloco}>
+          <Text style={styles.label}>Cidade: </Text>
+          <TextInput
+            placeholder='Digite aqui sua cidade'
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.bloco}>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.textoBotao}> Buscar </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+
+        </View>
     </View>
   );
 }
@@ -13,8 +29,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  titulo: {
+    marginTop: 30,
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  label:{
+    fontSize: 20,
+  },
+  bloco:{
+    marginTop: 30,
+    marginLeft: '20%',
+  },
+  input:{
+    borderBottomWidth: 2,
+    width:'80%',
+    fontSize: 20,
+  },
+  botao:{
+    width: '80%',
+    backgroundColor: '#FDDDDE',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: '2%',
+    borderRadius: 50
+  },
+  textoBotao:{
+    fontSize: 20,
+    color: '#00',
   },
 });
