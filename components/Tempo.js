e070a5f2a7f162829e8ec95f1ab0ff7c1700eacb
lígia <79666957+mimiyama@users.noreply.github.com>
2022-09-01 15:46:01 -0300

    Finalizando

--- components/Tempo.js
+++ components/Tempo.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 
 export default function Tempo(props){
     return(
         <>
-            <Text style={styles.texto}> Min: {props.data.min} </Text>
-            <Text style={styles.texto}> Max: {props.data.max}</Text>
-            <Text style={styles.texto}> Descrição: </Text>
+             <View style={styles.bloco}>
+                <Text style={styles.texto}>{props.dados.city_name}</Text>
+                <Text style={styles.texto}>Clima de hoje:</Text>
+                <Text style={styles.texto}>Temperatura atual: {props.dados.temp}</Text>
+                <Text style={styles.texto}>Min: {props.atuais.min}</Text>
+                <Text style={styles.texto}>Max: {props.atuais.max}</Text>
+                <Text style={styles.texto}>Descrição: {props.atuais.description}</Text>
+                <Text style={styles.texto}>Clima de amanhã: </Text>
+                <Text style={styles.texto}>Min: {props.depois.min}</Text>
+                <Text style={styles.texto}>Max: {props.depois.max}</Text>
+                <Text style={styles.texto}>Descrição: {props.depois.description}</Text>
+            </View>
         </>
     )
 }
@@ -15,6 +24,8 @@ const styles = StyleSheet.create({
     texto: {
         fontSize: 20,
         marginTop: 10,
-
     },
+    bloco: {
+        alignItems: 'center',
+    }
 });
